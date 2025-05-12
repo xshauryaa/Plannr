278c39ca3823505f444b954eac5fd6666ac9f01e
Shaurya Thareja <shauryathareja19@gmail.com>
2025-05-13 01:47:18 +0530

    Migrated TimeBlock & DaySchedule classes

---
+++ src/model/DaySchedule.js
@@ -0,0 +1,228 @@
+import ActivityType from './ActivityType';
+import ScheduleDate from './ScheduleDate';
+import TimeBlock from './TimeBlock';
+import Time24 from './Time24';
+import EventConflictError from './exceptions/EventConflictError';
+import WorkingLimitExceededError from './exceptions/WorkingLimitExceededError';
+
+/**
+ * Represents a schedule for a single day.
+ */
+class DaySchedule {
+  /**
+   * @param {string} day - Day of the week (e.g. "Monday", "Tuesday", ...)
+   * @param {ScheduleDate} date - The date for this day
+   * @param {number} minGap - Minimum gap between events in minutes
+   * @param {number} workingHoursLimit - Maximum working hours per day
+   */
+  constructor(day, date, minGap, workingHoursLimit) {
+    this.day = day;
+    this.date = date;
+    this.minGap = minGap;
+    this.workingHoursLimit = workingHoursLimit;
+    this.events = [];     // list of events (RigidEvent or FlexibleEvent)
+    this.breaks = [];     // list of break objects
+    this.timeBlocks = []; // list of TimeBlock objects
+  }
+
+  /** @returns {string} the day of the week */
+  getDay() {
+    return this.day;
+  }
+
+  /** @returns {ScheduleDate} the date */
+  getDate() {
+    return this.date;
+  }
+
+  /** @returns {Array} the list of events */
+  getEvents() {
+    return this.events;
+  }
+
+  /** @returns {Array} the list of breaks */
+  getBreaks() {
+    return this.breaks;
+  }
+
+  /** @returns {Array} the list of time blocks */
+  getTimeBlocks() {
+    return this.timeBlocks;
+  }
+
+  /** @returns {number} the minimum gap between events in minutes */
+  getMinGap() {
+    return this.minGap;
+  }
+
+  /** @returns {number} the maximum working hours per day */
+  getWorkingHoursLimit() {
+    return this.workingHoursLimit;
+  }
+
+  /**
+   * Sets the maximum working hours per day.
+   * @param {number} limit - Must be > 0.
+   */
+  setWorkingHoursLimit(limit) {
+    this.workingHoursLimit = limit;
+  }
+
+  /**
+   * Adds a rigid event (with fixed time) to the schedule.
+   * @param {RigidEvent} event 
+   */
+  addEvent(event) {
+    this.events.push(event);
+    this.timeBlocks.push(new TimeBlock(event));
+    this.sortSchedule();
+  }
+
+  /**
+   * Adds a flexible event (with specified start and end time) to the schedule.
+   * Throws errors if there is any conflict or if the working hours limit is exceeded.
+   * @param {FlexibleEvent} event 
+   * @param {number} startTime - Start time in 24-hour integer format (e.g., 930)
+   * @param {number} endTime - End time in 24-hour integer format (e.g., 1045)
+   * @throws {EventConflictError} if the event overlaps an existing event or break
+   * @throws {WorkingLimitExceededError} if adding the event exceeds the working hours limit
+   */
+  addFlexibleEvent(event, startTime, endTime) {
+    if (this._checkEventConflict(event, startTime, endTime)) {
+      throw new EventConflictError();
+    } else if (event.getDuration() + (this.calculateWorkingHours() * 60) > (this.workingHoursLimit * 60)) {
+      throw new WorkingLimitExceededError();
+    } else {
+      this.events.push(event);
+      this.timeBlocks.push(new TimeBlock(event, this.date, startTime, endTime));
+      this.sortSchedule();
+    }
+  }
+
+  /**
+   * Adds a break to the schedule.
+   * REQUIRES: No events must be added before adding a break.
+   * @param {Break} breakTime 
+   */
+  addBreak(breakTime) {
+    this.breaks.push(breakTime);
+    this.timeBlocks.push(new TimeBlock(breakTime, this.date));
+    this.sortSchedule();
+  }
+
+  /**
+   * Removes an event from the schedule.
+   * @param {Event} event 
+   */
+  removeEvent(event) {
+    this.events = this.events.filter(e => !e.equals(event));
+    // Remove corresponding time block by matching event name.
+    for (let i = 0; i < this.timeBlocks.length; i++) {
+      if (this.timeBlocks[i].getName() === event.getName()) {
+        this.timeBlocks.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  /**
+   * Removes a break from the schedule.
+   * @param {Break} breakTime 
+   */
+  removeBreak(breakTime) {
+    this.breaks = this.breaks.filter(b => !b.equals(breakTime)); // Assumes Break implements equals()
+    // Remove corresponding time block by matching break properties.
+    for (let i = 0; i < this.timeBlocks.length; i++) {
+      if (
+        this.timeBlocks[i].getName() === "Break" &&
+        this.timeBlocks[i].getStartTime().equals(breakTime.getStartTime()) &&
+        this.timeBlocks[i].getEndTime().equals(breakTime.getEndTime())
+      ) {
+        this.timeBlocks.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  /**
+   * Calculates the total working hours (based on events of type EDUCATION, MEETING, or WORK).
+   * @returns {number} Total working hours (in whole hours)
+   */
+  calculateWorkingHours() {
+    let totalWorkingMinutes = 0;
+    for (let event of this.events) {
+      if (
+        event.getType() === ActivityType.EDUCATION ||
+        event.getType() === ActivityType.MEETING ||
+        event.getType() === ActivityType.WORK
+      ) {
+        totalWorkingMinutes += event.getDuration();
+      }
+    }
+    return Math.floor(totalWorkingMinutes / 60);
+  }
+
+  /**
+   * Checks if the given flexible event conflicts with any existing event or break.
+   * Returns true if the event overlaps with any time block.
+   * @param {FlexibleEvent} event 
+   * @param {number} startTime - Start time in 24-hour format
+   * @param {number} endTime - End time in 24-hour format
+   * @returns {boolean} true if there is a conflict, false otherwise.
+   */
+  _checkEventConflict(event, startTime, endTime) {
+    const newStart = new Time24(startTime);
+    const newEnd = new Time24(endTime);
+    
+    for (let tb of this.timeBlocks) {
+      // Check for overlap. Two intervals [newStart, newEnd] and [tbStart, tbEnd] overlap if:
+      // newStart < tbEnd && newEnd > tbStart
+      if (newStart.isBefore(tb.getEndTime()) && newEnd.isAfter(tb.getStartTime())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Sorts the time blocks in chronological order by their start times.
+   */
+  sortSchedule() {
+    this.timeBlocks.sort((a, b) => {
+      if (a.getStartTime().isBefore(b.getStartTime())) return -1;
+      if (a.getStartTime().equals(b.getStartTime())) return 0;
+      return 1;
+    });
+  }
+
+  /**
+   * Makes DaySchedule iterable over its time blocks.
+   * @returns {Iterator<TimeBlock>}
+   */
+  [Symbol.iterator]() {
+    let index = 0;
+    const timeBlocks = this.timeBlocks;
+    return {
+      next() {
+        if (index < timeBlocks.length) {
+          return { value: timeBlocks[index++], done: false };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
+
+  /**
+   * @returns {string} A string representation of the day schedule.
+   */
+  toString() {
+    let result = `Day: ${this.day}\n`;
+    for (let tb of this.timeBlocks) {
+      result += tb.toString() + "\n";
+    }
+    return result;
+  }
+}
+
+export default DaySchedule;
---
+++ src/model/TimeBlock.js
@@ -0,0 +1,132 @@
+import Time24 from './Time24';
+import ActivityType from './ActivityType';
+
+/**
+ * Represents a block of time in a schedule.
+ * Can be a rigid event, flexible event, or break.
+ */
+class TimeBlock {
+  /**
+   * Constructs a TimeBlock from a RigidEvent.
+   * @param {RigidEvent} event 
+   */
+  constructor(event, date, startTime, endTime) {
+    if (event.getType && event.getStartTime && event.getEndTime) {
+      // RigidEvent
+      this.name = event.getName();
+      this.date = event.getDate();
+      this.activityType = event.getType();
+      this.startTime = event.getStartTime();
+      this.endTime = event.getEndTime();
+      this.duration = event.getDuration();
+      this.isCompleted = false;
+      this.type = 'rigid';
+    } else if (event.getDuration && typeof startTime === 'number' && typeof endTime === 'number') {
+      // FlexibleEvent
+      this.name = event.getName();
+      this.date = date;
+      this.activityType = event.getType();
+      this.startTime = new Time24(startTime);
+      this.endTime = new Time24(endTime);
+      this.duration = event.getDuration();
+      this.isCompleted = false;
+      this.type = 'flexible';
+    } else {
+      // Break
+      this.name = 'Break';
+      this.date = date;
+      this.startTime = event.getStartTime();
+      this.endTime = event.getEndTime();
+      this.duration = event.getDuration();
+      this.isCompleted = true;
+      this.type = 'break';
+    }
+  }
+
+  /** @returns {string} name of the time block */
+  getName() {
+    return this.name;
+  }
+
+  /** @returns {ScheduleDate} date of the time block */
+  getDate() {
+    return this.date;
+  }
+
+  /** @returns {string|undefined} activity type (undefined for breaks) */
+  getActivityType() {
+    return this.activityType;
+  }
+
+  /** @returns {Time24} start time */
+  getStartTime() {
+    return this.startTime;
+  }
+
+  /** @returns {Time24} end time */
+  getEndTime() {
+    return this.endTime;
+  }
+
+  /** @returns {string} the type of time block: 'rigid', 'flexible', or 'break' */
+  getType() {
+    return this.type;
+  }
+
+  /** @returns {number} duration of the time block */
+  getDuration() {
+    return this.duration;
+  }
+
+  /** @returns {boolean} true if completed */
+  isCompleted() {
+    return this.isCompleted;
+  }
+
+  /**
+   * Sets the completion status of this time block.
+   * @param {boolean} completed 
+   */
+  setCompleted(completed) {
+    this.isCompleted = completed;
+  }
+
+  /**
+   * @param {any} other 
+   * @returns {boolean} true if other block is equal to this one
+   */
+  equals(other) {
+    return (
+      other instanceof TimeBlock &&
+      this.name === other.getName() &&
+      this.date.equals(other.getDate()) &&
+      this.startTime.equals(other.getStartTime()) &&
+      this.endTime.equals(other.getEndTime()) &&
+      this.duration === other.getDuration() &&
+      this.type === other.getType()
+    );
+  }
+
+  /**
+   * @returns {string} human-readable summary
+   */
+  toString() {
+    let result = `${this.startTime.toString()} - ${this.endTime.toString()}: ${this.name}`;
+    if (this.type === 'break') {
+      return result;
+    }
+    result += ` (${this.activityType})`;
+    return result;
+  }
+
+  /**
+   * Generates a UID for this block.
+   * @returns {string} a unique identifier for the block
+   */
+  getUID() {
+    const uid = `${this.date.toString()}-${this.startTime.toString()}-${this.name.replace(/\s+/g, '_')}`;
+    return `${uid}@plannr.scheduler`.toLowerCase();
+  }
+}
+
+export default TimeBlock;
