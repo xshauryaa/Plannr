df941d02a765658c6c4744dc064985256d613d32
Shaurya Thareja <shauryathareja19@gmail.com>
2025-05-13 00:52:18 +0530

    Migrated Break class

---
+++ src/model/Break.js
@@ -0,0 +1,49 @@
+import Time24 from './Time24';
+
+/**
+ * Represents a break in the schedule.
+ */
+class Break {
+  /**
+   * @param {number} duration - The duration of the break in minutes
+   * @param {number} startTime - The start time of the break in 24-hour format (numeric)
+   * @param {number} endTime - The end time of the break in 24-hour format (numeric)
+   * EFFECTS: creates a new break with the given duration
+   */
+  constructor(duration, startTime, endTime) {
+    this.duration = duration;
+    this.startTime = new Time24(startTime);
+    this.endTime = new Time24(endTime);
+  }
+
+  /** @returns {number} the duration of the break */
+  getDuration() {
+    return this.duration;
+  }
+
+  /** @returns {Time24} the start time of the break */
+  getStartTime() {
+    return this.startTime;
+  }
+
+  /** @returns {Time24} the end time of the break */
+  getEndTime() {
+    return this.endTime;
+  }
+
+  /**
+   * Compares this break with another for equality
+   * @param {any} other 
+   * @returns {boolean} true if same duration, startTime, and endTime
+   */
+  equals(other) {
+    return (
+      other instanceof Break &&
+      this.duration === other.duration &&
+      this.startTime === other.startTime &&
+      this.endTime === other.endTime
+    );
+  }
+}
+
+export default Break;
