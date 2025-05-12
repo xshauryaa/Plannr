0cc4e9bdddfffd2fe5e3eacb8a52d14b98dfd2dc
Shaurya Thareja <shauryathareja19@gmail.com>
2025-05-13 01:34:18 +0530

    Migrated Priority enumeration

---
+++ src/model/Priority.js
@@ -0,0 +1,11 @@
+/**
+ * Represents priority levels for flexible events.
+ */
+const Priority = Object.freeze({
+    LOW: 'LOW',
+    MEDIUM: 'MEDIUM',
+    HIGH: 'HIGH',
+  });
+  
+  export default Priority;
+  
