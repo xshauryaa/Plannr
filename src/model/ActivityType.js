ad7a9f61d714a4aad346207a7eac0568612b3694
Shaurya Thareja <shauryathareja19@gmail.com>
2025-05-13 00:36:58 +0530

    Migrated ActivityType enumeration

---
+++ src/model/ActivityType.js
@@ -0,0 +1,16 @@
+/**
+ * Enumerates the different types of activities that can be scheduled.
+ */
+const ActivityType = Object.freeze({
+    PERSONAL: 'PERSONAL',
+    MEETING: 'MEETING',
+    WORK: 'WORK',
+    EVENT: 'EVENT',
+    EDUCATION: 'EDUCATION',
+    TRAVEL: 'TRAVEL',
+    RECREATIONAL: 'RECREATIONAL',
+    ERRAND: 'ERRAND',
+    OTHER: 'OTHER',
+  });
+  
+  export default ActivityType;
