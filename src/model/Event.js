07c4b7ac5b4630ae6f3bb318d5e5403c56161a1a
Shaurya Thareja <shauryathareja19@gmail.com>
2025-05-13 01:30:18 +0530

    Migrated Event class

---
+++ src/model/Event.js
@@ -0,0 +1,58 @@
+import ActivityType from './ActivityType';
+
+/**
+ * Represents an event or task to be scheduled in the calendar.
+ * Abstract base class.
+ */
+class Event {
+  /**
+   * @param {string} name - The name of the event
+   * @param {string} type - The activity type (from ActivityType)
+   * @param {number} duration - The duration of the event in minutes
+   */
+  constructor(name, type, duration) {
+    if (new.target === Event) {
+      throw new Error('Cannot instantiate abstract class Event directly.');
+    }
+
+    this.name = name;
+    this.type = type;
+    this.duration = duration;
+  }
+
+  /** @returns {string} the name of the event */
+  getName() {
+    return this.name;
+  }
+
+  /** @returns {string} the activity type of the event */
+  getType() {
+    return this.type;
+  }
+
+  /** @returns {number} the duration of the event in minutes */
+  getDuration() {
+    return this.duration;
+  }
+
+  /** @returns {string} a string representation of the event */
+  toString() {
+    return `${this.name} (${this.type})`;
+  }
+
+  /**
+   * Checks equality with another event
+   * @param {any} other 
+   * @returns {boolean}
+   */
+  equals(other) {
+    return (
+      other instanceof Event &&
+      this.name === other.name &&
+      this.type === other.type &&
+      this.duration === other.duration
+    );
+  }
+}
+
+export default Event;
